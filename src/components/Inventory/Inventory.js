f6c9c8dd5a9c11c954d1be19267e5f2b77091210
uzzal111 <uzzaldatta310@gmail.com>
2022-07-08 23:13:06 -0600

    added

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Inventory = () => {
   return (
     <div>
-      <h1>This is inventory page clear   </h1>
+      <h1>This is inventory page clear   first added</h1>
     </div>
   );
 };
