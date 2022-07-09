09836bc50014a6d2664718bc66ea5afa9539ef8c
uzzal111 <uzzaldatta310@gmail.com>
2022-07-09 00:10:16 -0600

    adddes3

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Inventory = () => {
   return (
     <div>
-      <h1>This is inventory page clear   first added g</h1>
+      <h1>This is inventory page clear   first added g Uzzal Datta</h1>
     </div>
   );
 };
